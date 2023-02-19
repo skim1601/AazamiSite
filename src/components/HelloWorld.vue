6d5b86737cdd591de23cd1d5799e9b82ac204de0
skim1601 <`kimsihy093@gmail.com``>
2023-02-19 02:16:41 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      A breakthrough device that empowers people with dementia. With just a simple command, "I forgot," it replays the last 10 seconds of voice, easing the frustration of forgotten memories. 
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&family=Phudu&display=swap');
+
+h1 {
+  font-weight: 500;
+  font-size: 3rem;
+  top: -10px;
+  margin: 3rem 0;
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-family: 'DM Sans';
+}
+
+.greetings h1{
+  font-family: "Orbitron";
+  font-size: min(5rem, 10vw);   
+  text-align: center;
+	background-image: linear-gradient(90deg, rgb(122, 202, 106),rgb(0, 255, 0));
+	background-clip: text;
+	-webkit-background-clip: text;
+	-moz-background-clip: text;
+	-webkit-text-fill-color: transparent; 
+	-moz-text-fill-color: transparent;
+}
+.greetings h1,
+.greetings h3 {
+  color: #fff;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+    margin: 0;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <WelcomeItem>
+    <br/><br/>
+    <template #heading>Documentation</template>
+    <h3>The inspiration behind Aazami comes from a personal experience with a loved one who had dementia. Witnessing the struggles and frustration of forgetting memories was heart-wrenching. It made us realize the need for a simple yet effective solution that could alleviate this issue. That's when the idea of Aazami was born - to create a device that could ease the burden of memory loss, not just for our family, but for millions of families worldwide. Our hope is that Aazami can help people with dementia cherish and relive their precious memories, and provide a small but significant sense of comfort in their daily lives. <a href="https://vuejs.org/" target="_blank" rel="noopener">Official Demo</a></h3>
+  </WelcomeItem>
+</template>
